27aebf9d32db6eb75be751435c62b4996c5f618c
Jaco Labuschagne <zodimo@gmail.com>
2023-01-13 19:12:18 +0200

    init the commands

--- src/query.ts
+++ src/query.ts
@@ -7,4 +7,54 @@ export class Query {
   static createWithCardanoCliBin(cardniCliBinPath = 'cardano-cli'): Query {
     return new Query(cardniCliBinPath);
   }
+
+  // protocol-parameters
+
+  protocolParameters() {
+    throw new Error('Not yet implemented!');
+  }
+  // tip
+  tip() {
+    throw new Error('Not yet implemented!');
+  }
+  // stake-pools
+  stakePools() {
+    throw new Error('Not yet implemented!');
+  }
+  // stake-distribution
+  stakeDistribution() {
+    throw new Error('Not yet implemented!');
+  }
+  // stake-address-info
+  stakeAddressInfo() {
+    throw new Error('Not yet implemented!');
+  }
+  // utxo
+  utxo() {
+    throw new Error('Not yet implemented!');
+  }
+  // ledger-state
+  ledgerState() {
+    throw new Error('Not yet implemented!');
+  }
+  // protocol-state
+  protocolState() {
+    throw new Error('Not yet implemented!');
+  }
+  // stake-snapshot
+  stakeSnapshot() {
+    throw new Error('Not yet implemented!');
+  }
+  // pool-params
+  poolParams() {
+    throw new Error('Not yet implemented!');
+  }
+  // leadership-schedule
+  leadershipSchedule() {
+    throw new Error('Not yet implemented!');
+  }
+  // kes-period-info
+  kesPeriodInfo() {
+    throw new Error('Not yet implemented!');
+  }
 }
